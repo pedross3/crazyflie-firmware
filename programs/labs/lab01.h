4749592fd2c1a27c0325fe44300caf7ab8edca21
pedross3 <pe.santos91@gmail.com>
2021-08-19 08:45:15 -0300

    Lab 01

--- programs/labs/lab01.h
+++ programs/labs/lab01.h
@@ -2,23 +2,51 @@
 #include "crazyflie.h"
 
 // Define all LEDs as digital output objects
+// Define red LED (right) as digital output object
+
+DigitalOut ledr(LED_RED_R, !false);
+DigitalOut ledb(LED_BLUE_L, !false);
+BusOut ledg(LED_GREEN_L, LED_GREEN_R);
 
 // Define all motors as PWM objects
+PwmOut motor1(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
+int h;
 
 // Main program
 int main()
-{
-    // Blink blue LED indicating inicialization (5 seconds)
-    
-    // Turn on red LEDs indicating motors are armed
-    
-    // Test all motors with different frequencies (to make different noises)
-    
-    // Turn off red LEDs indicating motors are disarmed
-    
-    // Blink green LEDs indicating end of program
+{// Main program
     while(true)
     {
+    // Blink blue LED indicating inicialization (5 seconds)
+    for (h = 0; h < 5; h++) 
+    {ledb = !ledb; wait(1.0);}
+    // Blink LED every second
+    for (h = 0; h < 5; h++) 
+    {ledb = !ledb; ledr = !ledr; wait(1.0/2);}
+
+    // Turn on red LEDs indicating motors are armed
+    ledr = 1;
+    // Test all motors with different frequencies (to make different noises)
+        // Turn motor on (20% power) and off every second
+
+        motor1 = 0.2; wait(0.5);
+        motor2 = 0.5; wait(0.5);
+        motor3 = 0.7; wait(0.5);
+        motor4 = 1.0; wait(0.5);
         
+        motor1 = 0; wait(0.5);
+        motor2 = 0; wait(0.5);
+        motor3 = 0; wait(0.5);
+        motor4 = 0; wait(0.5);
     }
+    // Turn off red LEDs indicating motors are disarmed
+    ledr = 0;
+    // Blink green LEDs indicating end of program
+    for (h = 0; h < 5; h++) 
+    {ledb = !ledb; wait(1.0);}
+
 }
+
