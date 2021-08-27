c204643201646f87221d8b8fe0fdc9e6f4e657d1
pedross3 <pe.santos91@gmail.com>
2021-08-26 21:59:19 -0300

    Lab02

--- programs/labs/lab02.h
+++ programs/labs/lab02.h
@@ -1,25 +1,32 @@
-#include "mbed.h"
-#include "crazyflie.h"
+# include "mbed.h"
+# include "crazyflie.h"
 
-// Lab for measuring angular velocity as PWM value varies.
+// Define motor 1 as PWM output object
+PwmOut motor (MOTOR3);
 
-// Define Motor1 as PWM object
-PwmOut motor1(MOTOR3);
+// Converts desired angular velocity (rad/s) to PWM signal (%)
+float control_motor (float omega_r)
+{
+    int PWM;
+    PWM = (omega_r*omega_r)*2.118e-11 - omega_r*2.543e-06 + 0.1482;
+    return PWM;
+}
 
 // Main program
-
-int main()
+int main ()
 {
-    motor1.period(1.0/500.0);
-    while(true)
-    {
-        // wait(5);
-        // 
-        // motor1 = 1;
-        // wait(5);
-        // motor1 = 0;
+    // Set PWM frequency to 500 Hz
+    motor.period (1.0/500.0);
 
-        // Parte 2:
-        
+
+    // End of program
+    while ( true )
+    {
+         // Turn on motor 1 with 1.000 rad/s for 0.5 s
+        motor = control_motor(1000.0);
+        wait (0.5);
+        // Turn off motor 1
+        motor = 0.0;
+        wait(0.5);
     }
 }
