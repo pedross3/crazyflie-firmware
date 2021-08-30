5c60240556883ee754b892607b005ff6e9770ff3
pedross3 <pe.santos91@gmail.com>
2021-08-30 14:50:16 -0300

    Lab02

--- programs/labs/lab02.h
+++ programs/labs/lab02.h
@@ -8,7 +8,7 @@ PwmOut motor (MOTOR3);
 float control_motor (float omega_r)
 {
     float PWM;
-    PWM = (omega_r*omega_r)*2.118e-11 - omega_r*2.543e-06 + 0.1482;
+    PWM = pow(omega_r,2)*1.724e-11 - omega_r*1.009e-06 + 0.009;
     return PWM;
 }
 
@@ -18,12 +18,13 @@ int main ()
     // Set PWM frequency to 500 Hz
     motor.period (1.0/500.0);
 
-
+    wait(5);
     // End of program
     while ( true )
     {
-         // Turn on motor 1 with 1.000 rad/s for 0.5 s
-        motor = control_motor(1000.0);
+         // Turn on motor 1 with 1.000 rad/s for 5 s
+        motor = control_motor(1000);
+        // motor = 1.0;
         wait (5);
         // Turn off motor 1
         motor = 0.0;
