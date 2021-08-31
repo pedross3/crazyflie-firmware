50659d5ceb5f696a96fc1b8f6b6e2da11c4f0572
pedross3 <pe.santos91@gmail.com>
2021-08-31 07:54:18 -0300

    Curve Fitting

--- programs/labs/lab02.h
+++ programs/labs/lab02.h
@@ -2,20 +2,20 @@
 # include "crazyflie.h"
 
 // Define motor 1 as PWM output object
-PwmOut motor (MOTOR3);
+PwmOut motor (MOTOR2);
 
 // Converts desired angular velocity (rad/s) to PWM signal (%)
 float control_motor (float omega_r)
 {
     float PWM;
-    PWM = pow(omega_r,2)*1.724e-11 - omega_r*1.009e-06 + 0.009;
+    PWM = omega_r*omega_r*1.184e-07 + omega_r*1.818e-12;
     return PWM;
 }
 
 // Main program
 int main ()
 {
-    // Set PWM frequency to 500 Hz
+    // Set PWM frequency to 500 Hzmatlab
     motor.period (1.0/500.0);
 
     wait(5);
@@ -24,7 +24,7 @@ int main ()
     {
          // Turn on motor 1 with 1.000 rad/s for 5 s
         motor = control_motor(1000);
-        // motor = 1.0;
+        // motor = 1.7421e-5;
         wait (5);
         // Turn off motor 1
         motor = 0.0;
